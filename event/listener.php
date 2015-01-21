02cb3c23baa02f66d561c310831476264d8f2b3f
Richard McGirr <rmcgirr83@gmail.com>
2015-01-21 05:44:46 -0500

    Fix Travis errors
    Damn whitespace

--- event/listener.php
+++ event/listener.php
@@ -21,7 +21,7 @@ class listener implements EventSubscriberInterface
 
 	/** @var string phpBB root path */
 	protected $phpbb_root_path;
-	
+
 	/** @var string PHP extension */
 	protected $php_ext;
 
@@ -46,16 +46,16 @@ class listener implements EventSubscriberInterface
 			// we need to ensure that what we are resetting is appropriate
 			// do we care about when someone edits the first post of a topic?
 			// $event['data']['topic_first_post_id'] == $event['data']['post_id'] $post_mode = 'edit_first_post'
-			
+
 			$ext_post_mode = '';
 			if($event['data']['topic_posts_approved'] + $event['data']['topic_posts_unapproved'] + $event['data']['topic_posts_softdeleted'] == 1)
 			{
 				$ext_post_mode = 'edit_topic';
 			}
-			elseif ($event['data']['topic_last_post_id'] == $event['data']['post_id'])
-				{
+			else if($event['data']['topic_last_post_id'] == $event['data']['post_id'])
+			{
 				$ext_post_mode = 'edit_last_post';
-				}
+			}
 
 			if($ext_post_mode == 'edit_last_post' || $ext_post_mode == 'edit_topic')
 			{
@@ -68,8 +68,8 @@ class listener implements EventSubscriberInterface
 				$sql_update_topics = 'UPDATE ' . TOPICS_TABLE . ' 
 					SET topic_last_post_time = ' . time() . ' 
 					WHERE topic_id = ' . $event['data']['topic_id'];
-				$this->db->sql_query($sql_update_topics);            
-			
+				$this->db->sql_query($sql_update_topics);           
+
 				if(!function_exists('markread'))
 				{
 					include ($this->phpbb_root_path . 'includes/functions_posting.' . $this->phpEx);
